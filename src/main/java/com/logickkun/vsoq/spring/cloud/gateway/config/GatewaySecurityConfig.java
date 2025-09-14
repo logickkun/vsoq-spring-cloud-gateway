230730b2f1552142381d042e91c6ae1340d4531a
logickkun <c.hyunmyung@gmail.com>
2025-09-15 00:58:42 +0900

    1. SAS 인증 플로우 구현  - localhost:8080/bff/web/login 으로 미인증자가 접속  - BFF layer에서 state/nonce/PKCE 생성 게이트웨이가 /:9000/oauth2/authorize?...로 프록시  - SAS(미인증) → 302 → /:9000/auth/login  -POST 요청으로 /:9000/web/login 프록시
     -

--- src/main/java/com/logickkun/vsoq/spring/cloud/gateway/config/GatewaySecurityConfig.java
+++ src/main/java/com/logickkun/vsoq/spring/cloud/gateway/config/GatewaySecurityConfig.java
@@ -2,8 +2,6 @@ package com.logickkun.vsoq.spring.cloud.gateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -17,38 +15,31 @@ public class GatewaySecurityConfig {
     @Bean
     public SecurityWebFilterChain gatewaySecurityFilterChain(ServerHttpSecurity http) {
         return http
-                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .csrf(ServerHttpSecurity.CsrfSpec::disable) // 최소화하려면 추후 /login POST만 예외로
                 .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)
                 .formLogin(ServerHttpSecurity.FormLoginSpec::disable)
-                .authorizeExchange(exchanges -> exchanges
+                .authorizeExchange(ex -> ex
                         .pathMatchers(
-                                // 미인증 로그인 관련 주소
-                                "/login",
-                                "/bff/web/login",
+                                // DevTools(F12) Endpoint
+                                "/.well-known/**",
+
+                                // Login Form Request Endpoint
+                                "/auth/login",
+
+                                //BFF WEB Login Request Endpoint
+                                "/bff/web/login", "/bff/web/callback",
+
+                                //Authorization Server Endpoint
+                                "/oauth2/authorize", "/oauth2/token", "/oauth2/revoke", "/oauth2/jwks", "/connect/register", "/connect/userinfo", "/connect/logout",
 
                                 // Static resources
-                                "/index.html",
-                                "/vite.svg",
-                                "/assets/**",
-                                "/favicon.ico"
+                                "/index.html","/assets/**","/favicon.ico","/vite.svg",
+
+                                // Error Page
+                                "/error"
                         ).permitAll()
-                        .pathMatchers("/", "/api/**").authenticated()
                         .anyExchange().authenticated()
                 )
-                // 인증 실패 시 /login 으로 리다이렉트
-                .exceptionHandling(ex -> ex
-                        .authenticationEntryPoint((exchange, e) -> {
-                            exchange.getResponse().setStatusCode(HttpStatus.TEMPORARY_REDIRECT);
-                            exchange.getResponse()
-                                    .getHeaders()
-                                    .setLocation(URI.create("/login"));
-                            return exchange.getResponse().setComplete();
-                        })
-                )
-                // 4) JWT 리소스 서버 설정 (jwt() → jwt(withDefaults())) gateway level에서 토큰 검증을 해주는 역할.
-//                .oauth2ResourceServer(oauth2 -> oauth2
-//                        .jwt(Customizer.withDefaults())
-//                )
                 .build();
     }
 }
